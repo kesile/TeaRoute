9ef475ded7c4a8f69bff7261313a160aafa37d69
Kessler <88524152+kesile@users.noreply.github.com>
2023-08-14 14:39:52 -0400

    Delete build directory

--- build/lib/TeaRoute/router/__init__.py
+++
@@ -1,6 +0,0 @@
-from .inner import Router
-from .methods.chat import chat
-from .methods.cos import cos
-from .methods.embed import embed
-from .methods.route import route
-from .methods.training import trainFile
--- build/lib/TeaRoute/router/methods/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chat import chat
-from .cos import cos
-from .embed import embed
-from .route import route
-from .training import trainFile
