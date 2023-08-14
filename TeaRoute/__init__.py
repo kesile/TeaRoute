9bdbac1136b5871ae350781e7d0594f4e9f87ee7
Kessler <88524152+kesile@users.noreply.github.com>
2023-08-14 14:39:37 -0400

    Delete TeaRoute directory

--- TeaRoute/__init__.py
+++
@@ -1,3 +0,0 @@
-from .main import Tear
-from .router.inner import Router
-from .router.methods import chat, cos, embed, route, training
--- TeaRoute/router/methods/embed.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-def embed(inp):
-    response = openai.Embedding.create(
-        input=inp,
-        model="text-embedding-ada-002"
-    )
-    embeddings = response['data'][0]['embedding']
-    return embeddings
